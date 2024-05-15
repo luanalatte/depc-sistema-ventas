953122d072da577e7e37d2ba20291d1932b70a6f
Luana Passafaro <luana.c.passafaro@gmail.com>
2024-05-15 11:32:18 -0300

    reporte.php

---
+++ reporte.php
@@ -0,0 +1,31 @@
+<?php
+
+$fecha = date("Y-m-d");
+
+header("Content-Type: text/csv; charset=utf-8");
+header("Content-Disposition: attachment; filename=reporte-$fecha.csv");
+
+include_once("config.php");
+include_once("entidades/venta.php");
+
+$aVentas = Venta::obtenerGrilla();
+
+$fs = fopen("php://output", "w");
+fputs($fs, $bom = chr(0xEF).chr(0xBB).chr(0xBF));
+
+$aTitulos = ["Fecha", "Cliente", "Producto", "Cantidad", "Total"];
+fputcsv($fs, $aTitulos, ";");
+
+foreach ($aVentas as $venta) {
+    $aFila = [
+        $venta->fecha_hora,
+        $venta->nombre_cliente,
+        $venta->nombre_producto,
+        $venta->cantidad,
+        $venta->total
+    ];
+
+    fputcsv($fs, $aFila, ";");
+}
+
+fclose($fs);
